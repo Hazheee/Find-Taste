51bd8e7adc06a879e6b0a23f4e716120bf1accb3
Hazheee <78984325+Hazheee@users.noreply.github.com>
2021-10-22 21:32:23 +0300

    Add files via upload

---
+++ MenuItems.js
@@ -0,0 +1,33 @@
+export const MenuItems=[
+    {
+        title:"Home",
+        url:"#",
+        cName:"nav-links"
+    },
+    {
+        title:"Restaurants",
+        url:"#",
+        cName:"nav-links"
+    },
+    {
+        title:"About",
+        url:"#",
+        cName:"nav-links"
+    },
+    {
+        title:"Categories",
+        url:"#",
+        cName:"nav-links"
+    },
+    {
+        title:"Restaurant locations",
+        url:"#",
+        cName:"nav-links"
+    },
+    {
+        title:"Sign in",
+        url:"#",
+        cName:"nav-links"
+    }
+];
+
